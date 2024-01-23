209488ebaddf2779fb72e966a64c4a1fe203f969
Philip Klempin <philip@klempin.se>
2024-01-23 12:54:54 +0100

    Add instance and community tags

--- init.php
+++ init.php
@@ -4,7 +4,7 @@ class Lemmy extends Plugin
 {
     function about()
     {
-        return [null, 'Add content to Lemmy feeds'];
+        return [null, 'Add content to Lemmy feeds', 'Philip Klempin', false, 'https://github.com/klempin/ttrss-lemmy'];
     }
 
     function api_version()
@@ -14,5 +14,26 @@ class Lemmy extends Plugin
 
     function init($host)
     {
+        $host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
+    }
+
+    function hook_article_filter($article)
+    {
+        $host = parse_url($article['feed']['fetch_url'], PHP_URL_HOST);
+        if (in_array($host, [
+            'feddit.de',
+            'lemm.ee',
+            'lemmy.world',
+            'sh.itjust.works',
+        ], true)) {
+            $article['tags'][] = $host;
+
+            preg_match('/href="https:\/\/' . $host . '\/c\/.+?"/', $article['content'], $matches);
+            if (!empty($matches[0])) {
+                $article['tags'][] = substr($matches[0], 17 + strlen($host), -1);
+            }
+        }
+
+        return $article;
     }
 }
