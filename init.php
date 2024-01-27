379b4f87c689855c04dc265ae0a2abff60fb34ac
Philip Klempin <philip@klempin.se>
2024-01-27 16:33:50 +0100

    Inline video and audio

--- init.php
+++ init.php
@@ -2,6 +2,30 @@
 
 class Lemmy extends Plugin
 {
+    private const LEMMY_DOMAINS = [
+        'ani.social',
+        'beehaw.org',
+        'derp.foo',
+        'discuss.tchncs.de',
+        'feddit.de',
+        'feddit.uk',
+        'lemm.ee',
+        'lemmy.dbzer0.com',
+        'lemmy.ml',
+        'lemmy.world',
+        'lemmy.zip',
+        'midwest.social',
+        'programming.dev',
+        'sh.itjust.works',
+        'sopuli.xyz',
+    ];
+
+    private const VIDEO_MIME_TYPES = [
+        'mov' => 'video/quicktime',
+        'mp4' => 'video/mp4',
+        'webm' => 'video/webm',
+    ];
+
     function about()
     {
         return [null, 'Add content to Lemmy feeds', 'Philip Klempin', false, 'https://github.com/klempin/ttrss-lemmy'];
@@ -19,20 +43,63 @@ class Lemmy extends Plugin
 
     function hook_article_filter($article)
     {
-        $host = parse_url($article['feed']['fetch_url'], PHP_URL_HOST);
-        if (in_array($host, [
-            'feddit.de',
-            'lemm.ee',
-            'lemmy.world',
-            'sh.itjust.works',
-        ], true)) {
-            $article['tags'][] = $host;
+        foreach ([
+            $article['feed']['fetch_url'] ?? null,
+            $article['feed']['site_url'] ?? null,
+            $article['site_url'] ?? null,
+            $article['comments'] ?? null,
+            $article['author'] ?? null,
+        ] as $uri) {
+            if (empty($uri)) {
+                continue;
+            }
 
-            preg_match('/href="https:\/\/' . $host . '\/c\/.+?"/', $article['content'], $matches);
-            if (!empty($matches[0])) {
-                $article['tags'][] = substr($matches[0], 17 + strlen($host), -1);
+            $host = parse_url($uri, PHP_URL_HOST);
+            if (in_array($host, static::LEMMY_DOMAINS, true)) {
+                $article = static::addTags($article, $host);
+                $article = static::inlineMedia($article);
+            }
+
+            break;
+        }
+
+        return $article;
+    }
+
+    private static function addTags(array $article, string $host): array
+    {
+        $article['tags'][] = $host;
+
+        preg_match('/href="https:\/\/' . $host . '\/c\/(.+?)\/?"/', $article['content'], $matches);
+        if (!empty($matches[1])) {
+            $article['tags'][] = $matches[1];
+        }
+
+        return $article;
+    }
+
+    private static function inlineMedia(array $article): array
+    {
+        preg_match_all('/href="(.+?)"/', $article['content'], $matches);
+        $inlineHtml = '';
+        foreach ($matches[1] ?? [] as $uri) {
+            $uriParts = parse_url($uri);
+            if ($uriParts === false) {
+                continue;
+            }
+
+            $pathinfo = pathinfo($uriParts['path'] ?? '');
+
+            if (array_key_exists($pathinfo['extension'] ?? null, static::VIDEO_MIME_TYPES)) {
+                $inlineHtml .= '<p><video preload="metadata" controls="true"><source src="' . htmlspecialchars($uri) . '" type="' . static::VIDEO_MIME_TYPES[$pathinfo['extension']] . '"></video></p>';
+            }
+
+            if (in_array($pathinfo['extension'] ?? null, ['gif', 'jpeg', 'jpg', 'png', 'webp'], true)) {
+                $inlineHtml .= '<p><img loading="lazy" src="' . htmlspecialchars($uri) . '"></p>';
             }
         }
+
+        $article['content'] = $inlineHtml . $article['content'];
 
         return $article;
     }
